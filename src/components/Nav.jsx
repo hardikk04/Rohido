bbfe31e9c8b3fde69c9bb2353d0da9efab74d62f
Hardik Sisodiya <hardik04062003@gmail.com>
2024-11-29 14:51:36 +0530

    active nav fixed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
-import { NavLink } from "react-router-dom";
 import gsap from "gsap";
 import { useState } from "react";
 import TransitionLink from "./TransitionLink";
+import { useLocation } from "react-router-dom";
 
 const Nav = () => {
   const [flag, setFlag] = useState(true);
+  const { pathname } = useLocation();
 
   return (
     <div className="fixed z-50 nav left-[50%] top-[2%] -translate-x-[50%] w-[95%] bg-white/5 rounded-[1.5vw] flex justify-between items-center px-[1.5vw] py-[1vw] border border-[#ffffff20] border-solid">
@@ -78,151 +79,126 @@ const Nav = () => {
       <div className="menu backdrop-blur sm:bg-black md:bg-black text-white pointer-events-none sm:border-[1px] opacity-0 sm:w-full md:w-full w-[16vw] absolute right-0 top-[105%] border-[2px] border-white/30 p-[1.5vw] flex flex-col gap-[1.5vw] rounded-xl sm:top-[120%] sm:left-0 sm:px-[3vw]">
         <div className="menu-navigation sm:flex sm:flex-col sm:gap-[4vw]">
           <TransitionLink href="/">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "text-[#fe492e] pointer-events-none" : "pointer-events-none"
-              }
+            <div
+              onMouseEnter={() => {
+                gsap.to(".home-h3 h3", {
+                  transform: "translateY(-100%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              onMouseLeave={() => {
+                gsap.to(".home-h3 h3", {
+                  transform: "translateY(0%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              className={`home-h3 ${
+                pathname === "/" ? `text-[#fe492e]` : ``
+              } sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden`}
             >
-              <div
-                onMouseEnter={() => {
-                  gsap.to(".home-h3 h3", {
-                    transform: "translateY(-100%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                onMouseLeave={() => {
-                  gsap.to(".home-h3 h3", {
-                    transform: "translateY(0%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                className="home-h3  sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden"
-              >
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Home</h3>
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Home</h3>
-              </div>
-            </NavLink>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Home</h3>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Home</h3>
+            </div>
           </TransitionLink>
           <TransitionLink href="/about">
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive ? "text-[#fe492e] pointer-events-none" : "pointer-events-none"
-              }
+            <div
+              onMouseEnter={() => {
+                gsap.to(".about-h3 h3", {
+                  transform: "translateY(-100%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              onMouseLeave={() => {
+                gsap.to(".about-h3 h3", {
+                  transform: "translateY(0%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              className={`about-h3 ${
+                pathname === "/about" ? `text-[#fe492e]` : ``
+              } sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden`}
             >
-              <div
-                onMouseEnter={() => {
-                  gsap.to(".about-h3 h3", {
-                    transform: "translateY(-100%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                onMouseLeave={() => {
-                  gsap.to(".about-h3 h3", {
-                    transform: "translateY(0%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                className="about-h3 sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden"
-              >
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
-                  About Us
-                </h3>
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
-                  About Us
-                </h3>
-              </div>
-            </NavLink>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
+                About Us
+              </h3>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
+                About Us
+              </h3>
+            </div>
           </TransitionLink>
           <TransitionLink href="/service">
-            <NavLink
-              to="/service"
-              className={({ isActive }) =>
-                isActive ? "text-[#fe492e] pointer-events-none" : "pointer-events-none"
-              }
+            <div
+              onMouseEnter={() => {
+                gsap.to(".services-h3 h3", {
+                  transform: "translateY(-100%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              onMouseLeave={() => {
+                gsap.to(".services-h3 h3", {
+                  transform: "translateY(0%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              className={`services-h3 ${
+                pathname === "/service" ? `text-[#fe492e]` : ``
+              } sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden`}
             >
-              <div
-                onMouseEnter={() => {
-                  gsap.to(".services-h3 h3", {
-                    transform: "translateY(-100%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                onMouseLeave={() => {
-                  gsap.to(".services-h3 h3", {
-                    transform: "translateY(0%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                className="services-h3 sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden"
-              >
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
-                  Services
-                </h3>
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
-                  Services
-                </h3>
-              </div>
-            </NavLink>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
+                Services
+              </h3>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
+                Services
+              </h3>
+            </div>
           </TransitionLink>
           <TransitionLink href="/work">
-            <NavLink
-              to="/work"
-              className={({ isActive }) =>
-                isActive ? "text-[#fe492e] pointer-events-none" : "pointer-events-none"
-              }
+            <div
+              onMouseEnter={() => {
+                gsap.to(".work-h3 h3", {
+                  transform: "translateY(-100%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              onMouseLeave={() => {
+                gsap.to(".work-h3 h3", {
+                  transform: "translateY(0%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              className={`work-h3 ${
+                pathname === "/work" ? `text-[#fe492e]` : ``
+              } sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden`}
             >
-              <div
-                onMouseEnter={() => {
-                  gsap.to(".work-h3 h3", {
-                    transform: "translateY(-100%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                onMouseLeave={() => {
-                  gsap.to(".work-h3 h3", {
-                    transform: "translateY(0%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                className="work-h3 sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden"
-              >
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Work</h3>
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Work</h3>
-              </div>
-            </NavLink>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Work</h3>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">Work</h3>
+            </div>
           </TransitionLink>
           <TransitionLink href="/contact">
-            <NavLink
-              to="/contact"
-              className={({ isActive }) =>
-                isActive ? "text-[#fe492e] pointer-events-none" : "pointer-events-none"
-              }
+            <div
+              onMouseEnter={() => {
+                gsap.to(".contact-h3 h3", {
+                  transform: "translateY(-100%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              onMouseLeave={() => {
+                gsap.to(".contact-h3 h3", {
+                  transform: "translateY(0%)",
+                  ease: "power1.inOut",
+                });
+              }}
+              className={`contact-h3 ${
+                pathname === "/contact" ? `text-[#fe492e]` : ``
+              } sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden`}
             >
-              <div
-                onMouseEnter={() => {
-                  gsap.to(".contact-h3 h3", {
-                    transform: "translateY(-100%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                onMouseLeave={() => {
-                  gsap.to(".contact-h3 h3", {
-                    transform: "translateY(0%)",
-                    ease: "power1.inOut",
-                  });
-                }}
-                className="contact-h3 sm:h-[8vw] md:h-[3vw] lg:h-[3.5vw] h-[2.5vw] overflow-hidden"
-              >
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
-                  Contact Us
-                </h3>
-                <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
-                  Contact Us
-                </h3>
-              </div>
-            </NavLink>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
+                Contact Us
+              </h3>
+              <h3 className="opacity-70 sm:opacity-100 sm:text-2xl">
+                Contact Us
+              </h3>
+            </div>
           </TransitionLink>
         </div>
         <div className="menu backdrop-blur bg-black absolute left-0 top-[103%] sm:top-[105%] sm:border-[1px] sm:w-full md:w-full w-[16vw] rounded-xl pointer-events-none border-[2px] border-white/30 p-[1.5vw] py-[.5vw] flex flex-col sm:px-[3vw]">
