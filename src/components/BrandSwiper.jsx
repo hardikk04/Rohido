ab7707e106d670b40d61715a133c2018ff6a73f5
Hardik Sisodiya <hardik04062003@gmail.com>
2024-11-29 15:28:48 +0530

    fixed brand swiper logo bug

--- src/components/BrandSwiper.jsx
+++ src/components/BrandSwiper.jsx
@@ -1,25 +1,23 @@
 const BrandSwiper = () => {
   return (
     <div className="brands-swiper overflow-hidden flex border-[2px] sm:border-[1px] border-white/40 px-[2vw]  rounded-[1.5vw] w-full md:h-[12vh] lg:h-[15vh] sm:h-[6vh] gap-[5vw]">
-      <div className="flex gap-[1vw] sm:gap-0 brands-ani">
+      <div className="flex gap-[6vw] sm:gap-0 brands-ani items-center">
 
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-1.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-0.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-2.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-1.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-0.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-2.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/amazon.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/American_Express_logo_(2018).svg.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/7-11.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/BoA.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/BARCLAY.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/cisco.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/google.png" alt="" />
 
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-1.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-0.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-2.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-1.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-0.png" alt="" />
-      <img className="object-contain sm:scale-100 scale-[.6]" src="/images/Examples-2.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/amazon.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/American_Express_logo_(2018).svg.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/7-11.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/BoA.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/BARCLAY.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/cisco.png" alt="" />
+      <img className="object-contain sm:scale-100 h-[8vw]" src="http://padams.in/images/client/google.png" alt="" />
       </div>
       {/* <div className="flex gap-[1vw] brands-ani sm:gap-0">
       <img className="object-contain scale-[.6] sm:scale-100" src="/images/Examples.png" alt="" />
