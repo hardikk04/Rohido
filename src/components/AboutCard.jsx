3c146d20edf0ff9d4bdb04171fc8c4e118c55827
Hardik Sisodiya <hardik04062003@gmail.com>
2024-12-02 11:46:19 +0530

    service card update

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -2,23 +2,30 @@ import gsap from "gsap";
 
 const AboutCard = ({ index, data }) => {
   const { title, para } = data;
-
+  const handleTouchStart = (event) => {
+    event.currentTarget.classList.toggle("hover");
+  };
   return (
-    <div
-      onMouseEnter={() => {
-        gsap.to(`.service-card-${index}`, {
-          backgroundImage: "linear-gradient(42deg, #000, #fe4a2e24)",
-        });
-      }}
-      onMouseLeave={() => {
-        gsap.to(`.service-card-${index}`, {
-          backgroundImage: "linear-gradient(143deg, #0c0c0d 23%, #222224)",
-        });
-      }}
-      className={`service-card-${index} service-cards md:mt-[1vw] sm:mt-[1vw] text-center about-cards relative z-[1] bg-black md:gap-[5vw] sm:w-full md:w-[49%] w-[24%] text-white rounded-lg shadow-md p-[1vw] py-[2vw] border-[2px] border-white/30`}
-    >
-      <h3 className="regular mb-[2vw]">{title}</h3>
-      <p className="opacity-70 light">{para}</p>
+    <div className="col service-cards" onTouchStart={handleTouchStart}>
+      <div className="container">
+        <div
+          className="front"
+          style={{
+            backgroundImage: 'url("https://unsplash.it/511/511/")',
+          }}
+        >
+          <div className="inner">
+            <p>{title}</p>
+          </div>
+        </div>
+        <div className="back">
+          <div className="inner">
+            <p>
+              {para}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
